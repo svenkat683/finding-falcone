197d176030614c7f33877b1088fb09ca7898f5b5
svenkat683 <svenkat683@gmail.com>
2019-02-21 11:38:34 +0530

    Add finding-falcone default and catch-all route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { FindingFalconeComponent } from "./components/finding-falcone/finding-falcone.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "finding-falcone",
+    pathMatch: "full"
+  },
+  {
+    path: "finding-falcone",
+    component: FindingFalconeComponent
+  },
+  {
+    path: "**",
+    redirectTo: "finding-falcone",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/finding-falcone/finding-falcone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-finding-falcone',
+  templateUrl: './finding-falcone.component.html',
+  styleUrls: ['./finding-falcone.component.css']
+})
+export class FindingFalconeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
