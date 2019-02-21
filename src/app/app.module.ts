eb3a3dfc5cc568d7405c99a2fdd850e20cbe1fa3
svenkat683 <svenkat683@gmail.com>
2019-02-21 14:14:15 +0530

    Add UI for selecting planet and vehicle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FindingFalconeComponent } from "./components/finding-falcone/finding-falcone.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { DestinationComponent } from "./components/destination/destination.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     FindingFalconeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DestinationComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/destination/destination.component.ts
+++ src/app/components/destination/destination.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import { Vehicle } from "src/app/models/vehicle";
+import { Planet } from "src/app/models/planet";
 
 @Component({
   selector: "app-destination",
@@ -7,6 +9,11 @@ import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
   styleUrls: ["./destination.component.css"]
 })
 export class DestinationComponent implements OnInit {
+  @Input() vehicles: Vehicle[];
+
+  @Input() planets: Planet[];
+
+  @Input() destinationIndex: number;
   constructor(private formBuilder: FormBuilder) {}
   destinationFormGroup: FormGroup;
   ngOnInit() {
