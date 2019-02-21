0598e595c90c4a90121b27aa35ac9aa8db916457
svenkat683 <svenkat683@gmail.com>
2019-02-21 12:06:59 +0530

    Add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FindingFalconeComponent } from "./components/finding-falcone/finding-falcone.component";
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, FindingFalconeComponent, HeaderComponent],
+  declarations: [AppComponent, FindingFalconeComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
