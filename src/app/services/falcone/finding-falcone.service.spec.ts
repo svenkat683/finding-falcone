7477a745351d63fa67969eb242494b760729a557
svenkat683 <svenkat683@gmail.com>
2019-02-22 22:49:03 +0530

    Add  getPlanets service test

--- src/app/services/falcone/finding-falcone.service.spec.ts
+++ src/app/services/falcone/finding-falcone.service.spec.ts
@@ -1,19 +1,52 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed, getTestBed } from "@angular/core/testing";
 import {
   HttpClientTestingModule,
   HttpTestingController
 } from "@angular/common/http/testing";
 import { FindingFalconeService } from "./finding-falcone.service";
+import { PlanetIntf } from "src/app/models/planet";
 
 describe("FindingFalconeService", () => {
-  beforeEach(() =>
+  let injector: TestBed;
+  let service: FindingFalconeService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
-    })
-  );
+      imports: [HttpClientTestingModule],
+      providers: [FindingFalconeService]
+    });
+
+    injector = getTestBed();
+    service = injector.get(FindingFalconeService);
+    httpMock = injector.get(HttpTestingController);
+  });
 
   it("should create FindingFalocne Service", () => {
     const service = TestBed.get(FindingFalconeService);
     expect(service).toBeTruthy();
+  });
+
+  describe("#getPlanets", () => {
+    it("should return an Observable<Planet[]>", () => {
+      const dummyResponse = [
+        { name: "Donlon", distance: 100 },
+        { name: "Enchai", distance: 200 },
+        { name: "Jebing", distance: 300 },
+        { name: "Sapir", distance: 400 },
+        { name: "Lerbin", distance: 500 },
+        { name: "Pingasor", distance: 600 }
+      ];
+      const apiBaseUrl = service.apiBaseUrl;
+
+      service.getPlanets().subscribe((planets: PlanetIntf[]) => {
+        expect(planets.length).toBe(6);
+        expect(planets).toEqual(dummyResponse);
+
+        const mockRequest = httpMock.expectOne(apiBaseUrl + "planets");
+        expect(mockRequest.request.method).toBe("GET");
+        mockRequest.flush(dummyResponse);
+      });
+    });
   });
 });
