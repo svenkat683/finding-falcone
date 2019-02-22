1d4397891208073b76e85aa151977b3c60bcf7f1
svenkat683 <svenkat683@gmail.com>
2019-02-22 21:46:11 +0530

    Add test to create FindingFalcone Service

--- src/app/services/falcone/finding-falcone.service.spec.ts
+++ src/app/services/falcone/finding-falcone.service.spec.ts
@@ -1,7 +1,19 @@
 import { TestBed } from "@angular/core/testing";
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 import { FindingFalconeService } from "./finding-falcone.service";
 
 describe("FindingFalconeService", () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    })
+  );
+
+  it("should create FindingFalocne Service", () => {
+    const service = TestBed.get(FindingFalconeService);
+    expect(service).toBeTruthy();
+  });
 });
