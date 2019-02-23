593d6bfd01232d15d1d7158bddf4367b40199249
svenkat683 <svenkat683@gmail.com>
2019-02-23 12:37:18 +0530

    Add tests to component ngOnInit

--- src/app/components/finding-falcone/finding-falcone.component.spec.ts
+++ src/app/components/finding-falcone/finding-falcone.component.spec.ts
@@ -4,11 +4,11 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { FindingFalconeComponent } from "./finding-falcone.component";
 import { DestinationComponent } from "../destination/destination.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FindingFalconeService } from "src/app/services/falcone/finding-falcone.service";
 
 describe("FindingFalconeComponent", () => {
   let component: FindingFalconeComponent;
   let fixture: ComponentFixture<FindingFalconeComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -29,5 +29,33 @@ describe("FindingFalconeComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should not have #planets, #vehicles, #availablePlanets and #token after construction", () => {
+    expect(component.planets).toBeUndefined();
+    expect(component.vehicles).toBeUndefined();
+    expect(component.availablePlanets).toBeUndefined();
+    expect(component.token).toBeUndefined();
+  });
+
+  it("#electedDestinations should be array with length zero after construction", () => {
+    expect(component.selectedDestinations.length).toBe(0);
+  });
+
+  describe("#ngOnInit()", () => {
+    it("Should call ngOnInit method", () => {
+      component.ngOnInit();
+      fixture.whenStable().then(() => {
+        spyOn(component, "getPlanets");
+        spyOn(component, "getVehicles");
+        spyOn(component, "getToken");
+        component.getPlanets();
+        component.getVehicles();
+        component.getToken();
+        expect(component.getPlanets).toHaveBeenCalled();
+        expect(component.getVehicles).toHaveBeenCalled();
+        expect(component.getToken).toHaveBeenCalled();
+      });
+    });
   });
 });
