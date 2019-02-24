b7894353e61de117efdc2292eccea1e6cd44ff1b
svenkat683 <svenkat683@gmail.com>
2019-02-24 08:44:24 +0530

    Add `validate the vehicle based on the their ranges` feature

--- src/app/components/destination/destination.component.ts
+++ src/app/components/destination/destination.component.ts
@@ -22,10 +22,13 @@ export class DestinationComponent implements OnInit, AfterViewChecked {
 
   @Input() planets: PlanetIntf[];
 
+  @Input() totalPlanets: PlanetIntf[];
+
   @Input() destinationIndex: number;
 
   @Output() selectedDestination = new EventEmitter<SelectedDestination>();
 
+  isVehicleValidErrorMessage = false;
   selectedPlanetName: string;
   constructor(
     private formBuilder: FormBuilder,
@@ -43,20 +46,48 @@ export class DestinationComponent implements OnInit, AfterViewChecked {
   ngAfterViewChecked() {
     this.changeDetector.detectChanges();
   }
+
   onSelectVehicle() {
-    const destination = new SelectedDestination(
-      this.destinationIndex,
-      this.destinationFormGroup.value.planetName,
-      this.destinationFormGroup.value.vehicle
-    );
     this.selectedPlanetName = this.destinationFormGroup.value.planetName;
     this.destinationFormGroup.patchValue({
       selectedPlanetName: this.destinationFormGroup.value.planetName
     });
-    this.emitSelectDestination(destination);
+    if (this.isVehicleValid()) {
+      const destination = new SelectedDestination(
+        this.destinationIndex,
+        this.destinationFormGroup.value.planetName,
+        this.destinationFormGroup.value.vehicle
+      );
+      this.emitSelectDestination(destination);
+    }
+    return;
   }
 
   emitSelectDestination(selectedDestination: SelectedDestination) {
     this.selectedDestination.emit(selectedDestination);
+  }
+
+  isVehicleValid() {
+    this.isVehicleValidErrorMessage = false;
+    const selectedVehicle = this.destinationFormGroup.value.vehicle;
+    const selectedPlanetName = this.destinationFormGroup.value.planetName;
+    console.log("planet", this.totalPlanets);
+    const vehicle: VehicleIntf = this.findPropertyByName(
+      selectedVehicle,
+      this.vehicles
+    );
+    const planet: PlanetIntf = this.findPropertyByName(
+      selectedPlanetName,
+      this.totalPlanets
+    );
+    if (vehicle.max_distance < planet.distance) {
+      this.isVehicleValidErrorMessage = true;
+      return false;
+    }
+    return true;
+  }
+
+  findPropertyByName(property: any, originalObject: any) {
+    return originalObject.find(object => object.name === property);
   }
 }
